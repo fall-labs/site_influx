a191aa7eb9542d800899d7a814f99599f4b1fd7f
Luíza Amanajás <luizamanajas@hotmail.com>
2025-08-20 14:52:51 -0300

    Footer V0.1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import { NavLink } from "react-router-dom";
+// import logo from "../assets/logo.svg";
+import classNames from "classnames";
+import "../assets/components/footer.scss";
+import parse from "html-react-parser";
+import wppIconPath from "../assets/images/whatsapp-icon.svg";
+import emailIconPath from "../assets/images/email-icon.svg";
+import addressIconPath from "../assets/images/address-icon.svg";
+
+const Footer = ({ contactInfo, aboutInfo }) => {
+  return (
+    <div id="footer" aria-label="Footer">
+      <div>
+        <div className="contact">
+          <h3>Contato</h3>
+          <div className="list">
+            <div>
+              <img src={wppIconPath} className="icon" />
+              <span>{parse(contactInfo.whatsapp)}</span>
+            </div>
+            <div>
+              <img src={emailIconPath} className="icon" />
+              <span>{parse(contactInfo.email)}</span>
+            </div>
+            <div>
+              <img src={addressIconPath} className="icon" />
+              <span>{parse(contactInfo.address)}</span>
+            </div>
+          </div>
+        </div>
+        <div className="about">
+          <h3>Sobre a Influx</h3>
+          <p className="intro">{aboutInfo.intro}</p>
+          <br />
+          <p>{parse(aboutInfo.text)}</p>
+        </div>
+      </div>
+      <div className="rights">
+        <p>© 2025 INFLUX Medição industrial. Todos os direitos reservados.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,18 +1,21 @@
-import "../assets/views/home.scss"
+import "../assets/views/home.scss";
 
 import NavBar from "../components/NavBar";
 import Container from "../components/Container";
-
+import Footer from "../components/Footer";
+import * as siteData from "../assets/data.json";
 
 function Home() {
-    return (
-        <>
-            <div className="main-background bg-dark">
-                <NavBar/>
-                <Container flexDirection="column"></Container>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="main-background bg-dark" style={{ height: "100vh" }}>
+        <NavBar />
+        <Container flexDirection="column"></Container>
+        <div class="separator"></div>
+        <Footer contactInfo={siteData.contact} aboutInfo={siteData.about} />
+      </div>
+    </>
+  );
 }
 
 export default Home;
