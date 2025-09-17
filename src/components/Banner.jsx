3a3661f72f5d499a1771a6a0b23b60a06eecf74e
Luíza Amanajás <luizamanajas@hotmail.com>
2025-09-17 20:08:50 -0300

    Banner update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,26 +1,33 @@
-import "../assets/components/banner.scss"
+import "../assets/components/banner.scss";
 import Container from "../components/Container";
 
 const Banner = () => {
-    return (
-        <div className="banner">
-        <Container>
-            <div className="banner-container">
-                <div className="banner-text1">
-                    <div className="title-text">Instrumentação Industrial</div>
-                    <div className="subtitle-text">Tecnologia aliada a um pós-venda especializado</div>
-                </div>
-                <div className="banner-text2">
-                    <div className="subtitle-text2">
-                        Soluções para medição de líquidos, vapor e gases<br/>
-                        Transmissor de pressão e nível<br/>
-                        Instrumentos analíticos de processo<br/>
-                        Serviços de calibração em campo com medidor ultrassônico ou skid móvel.
-                    </div>
-                </div>
+  return (
+    <div className="banner">
+      <Container>
+        <div className="banner-container">
+          <div className="banner-text1">
+            <div className="title-text">Instrumentação Industrial</div>
+            <div className="subtitle-text">
+              Tecnologia aliada a um pós-venda especializado
             </div>
-        </Container>
+          </div>
+          <div className="line-green"></div>
+          <div className="banner-text2">
+            <div className="subtitle-text2">
+              Soluções para medição de líquidos, vapor e gases
+              <br />
+              Transmissor de pressão e nível
+              <br />
+              Instrumentos analíticos de processo
+              <br />
+              Serviços de calibração em campo com medidor ultrassônico ou skid
+              móvel.
+            </div>
+          </div>
         </div>
+      </Container>
+    </div>
   );
 };
 
